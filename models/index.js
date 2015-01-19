6beefaf6b3b90758e49439ce3d6d97f34c3b9b75
Qi Tang <qitang@Qis-MacBook-Air.local>
2015-01-19 14:25:16 -0500

    use local db

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
-//mongoose.connect('mongodb://localhost/wikistack');
-mongoose.connect('mongodb://heroku_app31997271:7fhar9roop1b3b6pofksour5qi@ds053310.mongolab.com:53310/heroku_app31997271/Ieat');
+mongoose.connect('mongodb://localhost/wikistack');
+//mongoose.connect('mongodb://heroku_app31997271:7fhar9roop1b3b6pofksour5qi@ds053310.mongolab.com:53310/heroku_app31997271/Ieat');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
